cce2053ef233f50c0b0909b8d7cb8a8c9cc63bd0
angie-garciapaz <agarpaz06@gmail.com>
2026-03-22 21:27:30 -0600

    made sure to hide my secret url from google

---
+++ netlify/functions/bracket.js
@@ -0,0 +1,15 @@
+exports.handler = async () => {
+    const KEY      = process.env.SHEETS_API_KEY;
+    const SHEET_ID = '1RL-uJQzXWHlg2ck9qOOy-g2L02xnWLjf4GJaCkZ7pXc';
+    const TAB      = 'Dealer_Team_Bracket';
+
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${TAB}!A1:H500?key=${KEY}`;
+    const res  = await fetch(url);
+    const data = await res.json();
+
+    return {
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        body: JSON.stringify(data)
+    };
+};
